14d231cdad4b1c0271cea1aac06388621b474f50
Kyle Bai <k2r2.bai@gmail.com>
2023-10-31 15:02:34 +0800

    Update PassengerTypeRepositoryImpl.java

--- masterfile/src/main/java/com/caj/infra/repository/PassengerTypeRepositoryImpl.java
+++ masterfile/src/main/java/com/caj/infra/repository/PassengerTypeRepositoryImpl.java
@@ -3,6 +3,7 @@ package com.caj.infra.repository;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
